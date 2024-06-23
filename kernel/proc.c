4adaeef79aadf4cf41ebf1e58b50aac981497240
NancyAQ <nancyabuq@gmail.com>
2024-06-23 21:38:27 +0000

    modified page mapping to be iterative instead of one block

--- kernel/proc.c
+++ kernel/proc.c
@@ -691,26 +691,40 @@ procdump(void)
 //Assignment 3 functionn implementation
 uint64
 map_shared_pages(struct proc* src_proc,struct proc* dst_proc,uint64 src_va,uint64 size){
-  pte_t *pte;
-  src_va=PGROUNDDOWN(src_va);
-  size=PGROUNDUP(size);
-  if((pte=walk(src_proc->pagetable,src_va,size))==0){
-    panic("not valid pte");
-  }
-  if((*pte&PTE_V)==0){
-    panic("page doent exist");
-  }
-  if(((*pte&PTE_U)==0)){
-    panic("page not user accessible");
-  }
-  uint64 pa = PTE2PA(*pte); //physical address
-  uint64 a=dst_proc->sz+size-1;
-  uint flags;
-  flags = PTE_FLAGS(*pte);
-  flags|=PTE_S; //added shared flag
-  mappages(dst_proc->pagetable,a,size,pa,flags);
-  dst_proc->sz=dst_proc->sz+size;
-  return a;
+  //alligning va and size
+  uint64 first_page_va= PGROUNDDOWN(src_va);
+  uint64 last_page_va= PGROUNDUP(size+src_va);
+  uint64 added=last_page_va-first_page_va;
+  uint64 a;
+  a=PGROUNDUP(dst_proc->sz); //is rounding up needed?
+  uint curr_va;
+  //iterate over pages, find their pa using walk then map
+  for(curr_va=first_page_va;curr_va<last_page_va;curr_va=curr_va+PGSIZE){
+    pte_t *pte;
+    if((pte = walk(src_proc->pagetable,curr_va,0)) == 0){
+      panic("No such page");
+      return -1;
+    }
+    //correctness
+    if((*pte & PTE_V)==0){
+      panic("Not a valid address");
+      return -1;
+    }
+    if((*pte & PTE_U)==0){
+      panic("not accessible to user");
+      return -1;
+    }
+    uint64 pa=PTE2PA(*pte);
+    uint flags;
+    flags = PTE_FLAGS(*pte);
+    flags|=PTE_S; //added shared flag
+    //map to destination
+    mappages(dst_proc->pagetable,a,PGSIZE,pa,flags);// should I add sanity checks here?
+    a=a+PGSIZE;
+  } 
+  //update size and return va for source
+  dst_proc->sz=a;
+  return a-added; //new addr - added size=start of mapping
 }
 
 uint64
