8c88af35dbde09e6ec5cb2423cbc7dd365c77abb
NancyAQ <nancyabuq@gmail.com>
2024-06-30 13:41:34 +0000

    modified uvmunmap to only deallocate physical memory if it wasnt shared to the process

--- kernel/vm.c
+++ kernel/vm.c
@@ -183,7 +183,8 @@ uvmunmap(pagetable_t pagetable, uint64 va, uint64 npages, int do_free)
       panic("uvmunmap: not mapped");
     if(PTE_FLAGS(*pte) == PTE_V)
       panic("uvmunmap: not a leaf");
-    if(do_free){
+      //only unmaps physical mem if its not shared, ya3ni we own it
+    if(do_free &&((*pte & PTE_S)==0) ){ 
       uint64 pa = PTE2PA(*pte);
       kfree((void*)pa);
     }
